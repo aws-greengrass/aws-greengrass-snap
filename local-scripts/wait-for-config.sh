f4a77f0a33d8549f61b9ffc391ba1d4505d963a6
Tony Espy <espy@canonical.com>
2022-01-20 18:25:08 -0500

    [WIP] add basic JITP support
    This commit is an initial re-factoring of the
    v2 greengrass snap to:
    
     - not set a default config option
     - only run the installer once from the configure hook
     - automatically set the config option from the connect hook
     - setup a JITP inotify-based watcher from the connect hook
       if config.yaml isn't found
    
    Signed-off-by: Tony Espy <espy@canonical.com>

---
+++ local-scripts/wait-for-config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -ex
+#
+# Copyright 2022 Canonical, Inc.
+#
+# This script is used to wait for Greengrass credentials to be made available
+# by a configuration snap using the greengrass-config content interface. This
+# is necessary as devices using JITP (just-in-time-provisioning) do not have
+# device credentials built in the OS image, but instead receive them on first
+# boot via USB drive, or via remote RPC call to/from the device.
+#
+# This snap defines an content interface connect hook:
+#
+# connect_plug_greengrass_config_content
+#
+# ... which runs when the content interface between the greengrass snap and
+# the config provider snap is connected. If the file 'config.yaml' is found,
+# the connect hook sets the greengrass-config option, and starts the Nucleus.
+# If 'config.yaml' is not found, then a oneshot service which runs this script
+# is started.
+
+config_dir="$SNAP_DATA/greengrass/v2/shared-files"
+config_path="$config_dir/config.yaml"
+no_config=true
+
+while "$no_config"
+do
+  # how to watch for a specify file moved_to???
+  result=$(inotifywait -e moved_to -e close_write -q "$config_dir" | grep -o "config.yaml")
+  echo "result=$result"
+
+  if [ "$result" == "config.yaml" ]; then
+     no_config=false
+  fi
+done
+
+logger "gg:wait-for-config: setting greengrass-config=$config_path"
+snapctl set greengrass-config="$config_path"
+"$SNAP/snap/hooks/configure"
+
+echo "All DONE!!!"
+
+
