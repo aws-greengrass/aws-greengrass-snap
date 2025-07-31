6f46d51a2e29fc95977295a4888099a0a833b91a
Gary Emmerton <gemmerto@amazon.co.uk>
2025-07-31 09:12:31 +0100

    amd64 snap build

---
+++ local-scripts/connect.sh
@@ -0,0 +1,5 @@
+sudo snap connect aws-iot-greengrass:hardware-observe
+sudo snap connect aws-iot-greengrass:home
+sudo snap connect aws-iot-greengrass:system-observe
+sudo snap connect aws-iot-greengrass:mount-observe
+sudo snap connect aws-iot-greengrass:process-control
